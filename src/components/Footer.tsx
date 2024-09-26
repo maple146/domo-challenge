9187725069c7acd4d3d3a568330280843cb42734
Luciano Alcaraz <LGA-dev@hotmail.com>
2024-09-26 17:42:33 -0300

    Fix footer margin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="mt-5 mb-5 bg-white">
+    <footer className="mb-5 bg-white">
       <hr className="mt-5 mb-4" />
     </footer>
   );
