98539ce444fcf0a625633691fb238c26ad4f7a8c
Luciano Alcaraz <LGA-dev@hotmail.com>
2024-09-26 19:02:12 -0300

    Fix footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,5 @@
 const Footer: React.FC = () => {
-  return (
-    <footer className="mb-5 bg-white">
-      <hr className="mt-5 mb-4" />
-    </footer>
-  );
+  return <footer className="mb-5 bg-white"></footer>;
 };
 
 export default Footer;
